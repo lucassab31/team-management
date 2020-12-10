41ee676ad20bd93dd1158b062ff80123b2b43df0
maxime cervera <cervera.maxime@gmail.com>
2020-12-10 17:19:18 +0100

    Forms styling

--- matchs.php
+++ matchs.php
@@ -21,27 +21,27 @@
         </table>
     </section>
 
-    <section class="detail-macth">
+    <section class="detail-match">
         <h2 class="section-title">Match du XX/XX/XXXX à XX:XX</h2>
     </section>
 
-    <section class="ajout-macth">
-        <h2 class="section-title">Ajout d'un match</h2>
+    <section class="ajout-match">
+        <h2 class="section-title text-orange text-center">Ajout d'un match</h2>
         <form method="post">
-            <label for="date">Date du match</label>
-            <input type="date" name="dateM" required>
-            <label for="date">Heure du match</label>
-            <input type="time" name="heureM" required>
-            <label for="satut">Lieu</label>
+            <label for="date">Date du match</label></br>
+            <input type="date" name="dateM" required></br>
+            <label for="date">Heure du match</label></br>
+            <input type="time" name="heureM" required></br>
+            <label for="satut">Lieu</label></br>
             <select name="satut">
                 <option value="domicile">Domicile</option>
                 <option value="exterieur">Extérieur</option>
-            </select>
+            </select></br>
             <input type="submit" name="submitA" value="Ajouter">
         </form>
     </section>
 
-    <section class="modification-joueur">
+    <section class="modification-match">
     <h2 class="section-title">Modification du match du XX/XX/XXXX à XX:XX</h2>
         <form method="post">
         <label for="date">Date du match</label>
