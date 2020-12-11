2dd32b3750c7010d36974cc21346c5269d5f6b59
Lucas Sabathé <luqui31@gmail.com>
2020-12-11 09:19:53 +0100

    liste des match + ajout

--- matchs.php
+++ matchs.php
@@ -17,10 +17,29 @@
                 <tr>
                     <th>Date</th>
                     <th>L'heure</th>
+                    <th>opposant</th>
                     <th>Lieu</th>
                     <th>Score</th>
                     <th>Action</th>
                 </tr>
+                <?php
+                    $select = $bdd->query("SELECT * FROM matchs");
+                    while ($data = $select->fetch()) {
+                        ?>
+                        <tr>
+                            <td><?= $data['dateM'] ?></td>
+                            <td><?= $data['heureM'] ?></td>
+                            <td><?= $data['opposant'] ?></td>
+                            <td><?= $data['lieu'] ?></td>
+                            <td><?= isset($date['score']) ? $data['score'] : "NA" ?></td>
+                            <td>
+                                <a href="?modify=<?= $data['idMatch'] ?>"><i style="background-color:orange;" class="fas fa-pen"></i></a>
+                                <a href="?delete=<?= $data['idMatch'] ?>" onclick="Supp(this.href); return(false)"><i style="background-color:red;" class="fas fa-times"></i></a>
+                            </td>
+                        </tr>
+                        <?php
+                    }
+                ?>
             </table>
         </div>
     </section>
@@ -32,18 +51,26 @@
     <section class="ajout-match">
         <h2 class="section-title text-orange text-center">Ajout d'un match</h2>
         <form method="post">
-            <label for="date">Date du match</label></br>
-            <input type="date" name="dateM" required></br>
-            <label for="date">Heure du match</label></br>
-            <input type="time" name="heureM" required></br>
-            <label for="satut">Lieu</label></br>
-            <select name="satut">
+            <label for="dateM">Date du match</label>
+            <input type="date" name="dateM" required>
+            <label for="heureM">Heure du match</label>
+            <input type="time" name="heureM" required>
+            <input type="text" name="opposant" placeholder="Opposant" required>
+            <label for="lieu">Lieu</label>
+            <select name="lieu">
                 <option value="domicile">Domicile</option>
                 <option value="exterieur">Extérieur</option>
-            </select></br>
+            </select>
             <input type="submit" name="submitA" value="Ajouter">
         </form>
     </section>
+
+    <?php
+        if (isset($_POST['submitA'])) {
+            $insert = $bdd->prepare("INSERT INTO matchs(dateM, heureM, opposant, lieu) VALUES(?, ?, ?, ?)");
+            $insert->execute(array($_POST['dateM'], $_POST['heureM'], $_POST['opposant'], $_POST['lieu']));
+        }
+    ?>
 
     <section class="modification-match">
     <h2 class="section-title text-orange text-center">Modification du match du XX/XX/XXXX à XX:XX</h2>
@@ -52,8 +79,9 @@
             <input type="date" name="dateM" required>
             <label for="date">Heure du match</label>
             <input type="time" name="heureM" required>
-            <label for="satut">Lieu</label>
-            <select name="satut">
+            <input type="text" name="opposant" placeholder="Opposant" required>
+            <label for="lieu">Lieu</label>
+            <select name="lieu">
                 <option value="domicile">Domicile</option>
                 <option value="exterieur">Extérieur</option>
             </select>
